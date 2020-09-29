6f6861b766436f8a641e593ae0f941c6285929b1
DataPsycho <mr.data.psycho@gmail.com>
2020-09-29 23:46:10 +0200

    Changes: Test; Message: Adding Tests for Collector Structs

--- src/TwiliteTimeline.jl
+++ src/TwiliteTimeline.jl
@@ -1,5 +1,9 @@
 module TwiliteTimeline
 
+include("collector.jl")
+
+export Authentictor, ResourceParams
+
 # Write your package code here.
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using TwiliteTimeline
 using Test
 
-@testset "TwiliteTimeline.jl" begin
+# @testset "TwiliteTimeline.jl" begin
+#     # Write your tests here.
+# end
+
+@testset "collector.jl" begin
     # Write your tests here.
+    @testset "Validate Types" begin
+        creds = Authentictor("asdf", "asdf", "asdf", "asdf")
+        @test isa(creds, Authentictor)
+        resource_params = ResourceParams("realDonaldTrump", count=100)
+        @test isa(resource_params, ResourceParams)
+    end
 end
